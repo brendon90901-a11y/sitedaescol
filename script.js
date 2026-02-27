3e7ddf15190c141fe678faeecd6e15ea76878246
marcosr1 <marcosrichelly@gmail.com>
2026-02-27 18:37:35 -0300

    definitivo(talvez) quase pronto, 70%

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-function openGalleryTab(evt, tabName){
-  var i, content, buttons;
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("search-toggle");
+  const container = document.getElementById("search-container");
+  const form = container.querySelector("form");
 
-  content=document.getElementsByClassName("g-content");
-  for(i=0;i<content.length;i++){
-    content[i].style.display="none";
-  }
+  let aberto = false;
+  document.getElementById("ano").textContent = new Date().getFullYear();
 
-  buttons=document.getElementsByClassName("g-tab");
-  for(i=0;i<buttons.length;i++){
-    buttons[i].classList.remove("active");
-  }
+  function openGalleryTab(evt, tabName){
+    var i, content, buttons;
 
-  document.getElementById(tabName).style.display="block";
-  evt.currentTarget.classList.add("active");
-}
+    content=document.getElementsByClassName("g-content");
+    for(i=0;i<content.length;i++){
+      content[i].style.display="none";
+    }
 
-document.getElementById("ano").textContent = new Date().getFullYear();
+    buttons=document.getElementsByClassName("g-tab");
+    for(i=0;i<buttons.length;i++){
+      buttons[i].classList.remove("active");
+    }
 
-const toggleBtn = document.getElementById("search-toggle");
-const container = document.getElementById("search-container");
-const form = container.querySelector("form");
+    document.getElementById(tabName).style.display="block";
+    evt.currentTarget.classList.add("active");
+  } 
 
-let aberto = false;
-
-toggleBtn.addEventListener("click", () => {
-  if (!aberto) {
-    container.style.display = "block";
-    setTimeout(() => {
-      form.style.width = "250px";
-    }, 10);
-  } else {
-    form.style.width = "0";
-    setTimeout(() => {
-      container.style.display = "none";
-    }, 400);
-  }
-  aberto = !aberto;
+  toggleBtn.addEventListener("click", () => {
+    if (!aberto) {
+      container.style.display = "block";
+      setTimeout(() => {
+        form.style.width = "250px";
+      }, 10);
+    } else {
+      form.style.width = "0";
+      setTimeout(() => {
+        container.style.display = "none";
+      }, 400);
+    }
+    aberto = !aberto;
+  });
 });
